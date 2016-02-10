a9c36cc26b9b66717d7b09450b974ecf9028573e
Justen Morgan <justenamorgan@gmail.com>
2016-02-10 15:41:18 +0000

    Script

---
+++ sendemail.py
@@ -0,0 +1,55 @@
+import smtplib
+
+fromaddr = 'info3180.justenmorgan@gmail.com'
+
+fromname = "Justen Morgan"
+
+toaddr  = 'justenamorgan@gmail.com'
+
+toname = "Justen Morgan"
+
+msg = "Messaging message"
+
+subject = "Subject Message"
+
+message = """From: {} <{}>
+
+To: {} <{}>
+
+Subject: {}
+
+{}
+
+"""
+
+messagetosend = message.format(
+
+                             fromname,
+
+                             fromaddr,
+
+                             toname,
+
+                             toaddr,
+
+                             subject,
+
+                             msg)
+
+# Credentials (if needed)
+
+username = 'info3180.justenmorgan@gmail.com'
+
+password = 'info3180'
+
+# The actual mail send
+
+server = smtplib.SMTP('smtp.gmail.com:587')
+
+server.starttls()
+
+server.login(username,password)
+
+server.sendmail(fromaddr, toaddr, messagetosend)
+
+server.quit()
